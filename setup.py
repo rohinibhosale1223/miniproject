159a5ff483ca73dab6182049f3429997dc8d5e4c
rohinibhosale1223 <rohinibhosale1223@gmail.com>
2024-12-26 10:27:21 +0530

    “msg”

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mini project',
+    author='Rohini Bhosale',
+    license='',
+)
